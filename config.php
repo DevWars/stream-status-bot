748baa572465a661866bca5f5e51fd159bf8fc88
Daniel Peukert <dan.peukert@gmail.com>
2016-02-25 06:23:11 +0100

    various stuff

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+// Reddit
+$subreddit =	''; // e.g. DevWars
+$username =		'';
+$password =		'';
+$auth =			''; // REDDIT_APP_ID:REDDIT_APP_SECRET
+
+// Slack
+$slackurl =		'';
+
+// Email
+// $emailto =	'';
+// $emailfrom =	'';
+
+// Github
+$github_repo =	''; // e.g. DevWars/reddit-css
+$secret =		'';
+?>
--- index.php
+++ index.php
@@ -6,14 +6,9 @@
  * Licensed under GPL v3
  */
 
-/* SETTINGS */
+require_once 'config.php';
 
-require 'credentials.php';
-$subreddit = 'DevWars';
-$useragent = 'User-Agent: '.$subreddit.'_bot/1.0 by '.$username;
-
-
-/* ERROR LOGGING/HANDLING */
+$useragent = $subreddit.'_bot/1.0 by '.$username;
 
 function log_error($message, $quit = true)
 {
@@ -42,51 +37,53 @@ function log_error($message, $quit = true)
 	}
 }
 
-
-/* OAUTH */
-
+// AUTHENTICATING WITH REDDIT
 $login = curl_init();
-curl_setopt($login, CURLOPT_URL, 'https://ssl.reddit.com/api/v1/access_token');
-curl_setopt($login, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($login, CURLOPT_POST, TRUE);
-curl_setopt($login, CURLOPT_USERPWD, $auth);
-curl_setopt($login, CURLOPT_POSTFIELDS, array(
-                                                'grant_type'=>'password',
-                                                'username'=>$username,
-                                                'password'=>$password
-                                            ));
+$loginoptions = array(
+	CURLOPT_URL => 'https://ssl.reddit.com/api/v1/access_token',
+	CURLOPT_RETURNTRANSFER => 1,
+	CURLOPT_POST => true,
+	CURLOPT_USERPWD => $auth,
+	CURLOPT_POSTFIELDS => array(
+		'grant_type' => 'password',
+	    'username' => $username,
+		'password' => $password
+	)
+);
+curl_setopt_array($login, $loginoptions);
 $loginoutput = json_decode(curl_exec($login));
 $loginerror = curl_errno($login);
 curl_close($login);
 if($loginerror)
 {
-    log_error('When authenticating, there was a cURL error code: '.$loginerror);
+    log_error('Reddit authentication failed, cURL error code '.$loginerror);
 }
 $token = $loginoutput->access_token;
 
-
-/* GET CURRENT SETTINGS */
-
-$srsettings = curl_init();
-curl_setopt($srsettings, CURLOPT_URL, 'https://oauth.reddit.com/r/'.$subreddit.'/about/edit.json');
-curl_setopt($srsettings, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($srsettings, CURLOPT_HTTPHEADER,array(
-                                                $useragent,
-                                                'Authorization: bearer '.$token
-                                            ));
-$srsettingsoutput = json_decode(curl_exec($srsettings));
-$srsettingserror = curl_errno($srsettings);
-curl_close($srsettings);
-if($srsettingserror)
+// GETTTING CURRENT SETTINGS
+$settings = curl_init();
+$settingsoptions = array(
+	CURLOPT_URL => 'https://oauth.reddit.com/r/'.$subreddit.'/about/edit.json',
+	CURLOPT_RETURNTRANSFER => 1,
+	CURLOPT_HTTPHEADER => array(
+		'User-Agent: '.$useragent,
+        'Authorization: bearer '.$token
+	));
+);
+curl_setopt_array($settings, $settingsoptions);
+$settingsoutput = json_decode(curl_exec($settings));
+$settingserror = curl_errno($settings);
+curl_close($settings);
+if($settingserror)
 {
-    log_error('When getting subreddit settings, there was a cURL error code: '.$srsettingserror);
+    log_error('Getting subreddit settings failed, cURL error code '.$settingserror);
 }
-if(!$srsettingsoutput->data)
+if(!$settingsoutput->data)
 {
-    log_error('When getting subreddit settings, there was an error.');
+    log_error('Getting subreddit settings failed');
 }
-$srsettingsoutput = $srsettingsoutput->data;
-$linkcount = substr_count($srsettingsoutput->description, '[](#devwars)');
+$settingsoutput = $settingsoutput->data;
+$linkcount = substr_count($settingsoutput->description, '[](#devwars)');
 if($linkcount != 2)
 {
     if($linkcount == 0)
@@ -103,10 +100,8 @@ if($linkcount != 2)
     }
 }
 
-
-/* PROCESS AND USE DEVWARS AND TWITCH DATA */
-
-$descriptionparts = explode('[](#devwars)', $srsettingsoutput->description);
+// PROCESSING AND USING DEVWARS AND TWITCH DATA
+$sidebarparts = explode('[](#devwars)', $settingsoutput->description);
 
 $nextgame = json_decode(file_get_contents('http://devwars.tv/v1/game/nearestgame'))->timestamp;
 
@@ -114,69 +109,70 @@ $twitchdata = json_decode(file_get_contents('https://api.twitch.tv/kraken/stream
 
 if($twitchdata->stream != NULL)
 {
-	$descriptionparts[1] = '[● DEVWARS LIVE](http://www.twitch.tv/DevWars)';
+	$sidebarparts[1] = '[● DEVWARS LIVE](http://www.twitch.tv/DevWars)';
 }
 else if($nextgame)
 {
 	date_default_timezone_set('UTC');
-	$descriptionparts[1] = '**Next DevWars:**[](#linebreak) *'.date('l, F j - g:i A e',$nextgame/1000).'*';
+	$sidebarparts[1] = '**Next DevWars:**[](#linebreak) *'.date('l, F j - g:i A e',$nextgame/1000).'*';
 }
 else
 {
-	$descriptionparts[1] = '**Next DevWars:**[](#linebreak) *Unavailable*';
+	$sidebarparts[1] = '**Next DevWars:**[](#linebreak) *Unavailable*';
 }
 
-$descriptionparts = implode('[](#devwars)', $descriptionparts);
+$sidebarparts = implode('[](#devwars)', $sidebarparts);
 
-
-/* SEND NEW SETTINGS */
-
-$srsidebar = curl_init();
-curl_setopt($srsidebar, CURLOPT_URL, 'https://oauth.reddit.com/api/site_admin');
-curl_setopt($srsidebar, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($srsidebar, CURLOPT_HTTPHEADER, array(
-                                                $useragent,
-                                                'Authorization: bearer '.$token
-                                            ));
-curl_setopt($srsidebar, CURLOPT_POSTFIELDS, array(
-                                                'allow_top'=>                 $srsettingsoutput->default_set,
-                                                'api_type'=>                  'json',
-                                                'collapse_deleted_comments'=> $srsettingsoutput->collapse_deleted_comments,
-                                                'comment_score_hide_mins'=>   $srsettingsoutput->comment_score_hide_mins,
-                                                'css_on_cname'=>              $srsettingsoutput->domain_css,
-                                                'description'=>               $descriptionparts,
-                                                'exclude_banned_modqueue'=>   $srsettingsoutput->exclude_banned_modqueue,
-                                                'header-title'=>              $srsettingsoutput->header_hover_text,
-                                                'hide_ads'=>                  $srsettingsoutput->hide_ads,
-                                                'key_color'=>                 $srsettingsoutput->key_color,
-                                                'lang'=>                      $srsettingsoutput->language,
-                                                'link_type'=>                 $srsettingsoutput->content_options,
-                                                'name'=>                      $subreddit,
-                                                'over_18'=>                   $srsettingsoutput->over_18,
-                                                'public_description'=>        $srsettingsoutput->public_description,
-                                                'public_traffic'=>            $srsettingsoutput->public_traffic,
-                                                'show_cname_sidebar'=>        $srsettingsoutput->domain_sidebar,
-                                                'show_media'=>                $srsettingsoutput->show_media,
-                                                'spam_comments'=>             $srsettingsoutput->spam_comments,
-                                                'spam_links'=>                $srsettingsoutput->spam_links,
-                                                'spam_selfposts'=>            $srsettingsoutput->spam_selfposts,
-                                                'sr'=>                        $srsettingsoutput->subreddit_id,
-                                                'submit_link_label'=>         $srsettingsoutput->submit_link_label,
-                                                'submit_text'=>               $srsettingsoutput->submit_text,
-                                                'submit_text_label'=>         $srsettingsoutput->submit_text_label,
-                                                'suggested_comment_sort'=>    $srsettingsoutput->suggested_comment_sort,
-                                                'title'=>                     $srsettingsoutput->title,
-                                                'type'=>                      $srsettingsoutput->subreddit_type,
-                                                'wiki_edit_age'=>             $srsettingsoutput->wiki_edit_age,
-                                                'wiki_edit_karma'=>           $srsettingsoutput->wiki_edit_karma,
-                                                'wikimode'=>                  $srsettingsoutput->wikimode
-                                            ));
-$uploadoutput = json_decode(curl_exec($srsidebar));
-$srsidebarerror = curl_errno($srsidebar);
-curl_close($srsidebar);
-if($srsidebarerror)
+// UPLOADING NEW SETTINGS
+$sidebar = curl_init();
+$sidebaroptions = array(
+	CURLOPT_URL => 'https://oauth.reddit.com/api/site_admin',
+	CURLOPT_RETURNTRANSFER => 1,
+	CURLOPT_HTTPHEADER => array(
+		'User-Agent: '.$useragent,
+        'Authorization: bearer '.$token
+	),
+	CURLOPT_POSTFIELDS => array(
+		'allow_top'=>                 $srsettingsoutput->default_set,
+		'api_type'=>                  'json',
+		'collapse_deleted_comments'=> $srsettingsoutput->collapse_deleted_comments,
+		'comment_score_hide_mins'=>   $srsettingsoutput->comment_score_hide_mins,
+		'css_on_cname'=>              $srsettingsoutput->domain_css,
+		'description'=>               $sidebarparts,
+		'exclude_banned_modqueue'=>   $srsettingsoutput->exclude_banned_modqueue,
+		'header-title'=>              $srsettingsoutput->header_hover_text,
+		'hide_ads'=>                  $srsettingsoutput->hide_ads,
+		'key_color'=>                 $srsettingsoutput->key_color,
+		'lang'=>                      $srsettingsoutput->language,
+		'link_type'=>                 $srsettingsoutput->content_options,
+		'name'=>                      $subreddit,
+		'over_18'=>                   $srsettingsoutput->over_18,
+		'public_description'=>        $srsettingsoutput->public_description,
+		'public_traffic'=>            $srsettingsoutput->public_traffic,
+		'show_cname_sidebar'=>        $srsettingsoutput->domain_sidebar,
+		'show_media'=>                $srsettingsoutput->show_media,
+		'spam_comments'=>             $srsettingsoutput->spam_comments,
+		'spam_links'=>                $srsettingsoutput->spam_links,
+		'spam_selfposts'=>            $srsettingsoutput->spam_selfposts,
+		'sr'=>                        $srsettingsoutput->subreddit_id,
+		'submit_link_label'=>         $srsettingsoutput->submit_link_label,
+		'submit_text'=>               $srsettingsoutput->submit_text,
+		'submit_text_label'=>         $srsettingsoutput->submit_text_label,
+		'suggested_comment_sort'=>    $srsettingsoutput->suggested_comment_sort,
+		'title'=>                     $srsettingsoutput->title,
+		'type'=>                      $srsettingsoutput->subreddit_type,
+		'wiki_edit_age'=>             $srsettingsoutput->wiki_edit_age,
+		'wiki_edit_karma'=>           $srsettingsoutput->wiki_edit_karma,
+		'wikimode'=>                  $srsettingsoutput->wikimode
+	)
+);
+curl_setopt_array($sidebar, $sidebaroptions);
+$sidebaroutput = json_decode(curl_exec($sidebar));
+$srsidebarerror = curl_errno($sidebar);
+curl_close($sidebar);
+if($sidebarerror)
 {
-    log_error('When uploading changed subreddit settings, there was a cURL error code: '.$srsidebarerror);
+    log_error('Uploading subreddit settings failed, cURL error code '.$settingserror);
 }
 echo 'Success!';
 ?>
