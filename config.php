46905bb81827aa56b9c510d28ff08ad31c2b6c60
Daniel Peukert <dan.peukert@gmail.com>
2016-02-25 08:09:39 +0100

    Fixed spacing

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 // Reddit
-$subreddit =	''; // e.g. DevWars
-$username =		'';
-$password =		'';
-$auth =			''; // REDDIT_APP_ID:REDDIT_APP_SECRET
+$subreddit =    ''; // e.g. DevWars
+$username =     '';
+$password =     '';
+$auth =         ''; // REDDIT_APP_ID:REDDIT_APP_SECRET
 
 // Slack
-$slackurl =		'';
+$slackurl =     '';
 
 // Email
-// $emailto =	'';
-// $emailfrom =	'';
+// $emailto =   '';
+// $emailfrom = '';
 ?>
