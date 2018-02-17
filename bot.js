6ebce25386f344195329d69a0338a652029906aa
Daniel Peukert <dan.peukert@gmail.com>
2018-02-17 16:24:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 const snoowrap = require("snoowrap");
-const request = require("request");
-
+const fetch = require("node-fetch");
 const c = require("./config");
 const p = require("./package");
+
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
@@ -14,36 +14,42 @@ const r = new snoowrap({
 	clientSecret: c.reddit.clientsecret
 });
 
-r.getSubreddit(c.reddit.subreddit).getSettings().then(settings => {
-	var sidebar = settings.description.split("[](#" + c.reddit.subreddit + ")");
+let sidebar;
 
-	if(sidebar.length != 3) return;
-
-	request({
-		url: "https://api.twitch.tv/kraken/streams/" + c.twitch.channel,
-		json: true,
-		headers: {
-			"Accept": "application/vnd.twitchtv.v5+json",
-			"Client-ID": c.twitch.clientid
-		}
-	}, (error, response, body) => {
-		if(!error && response.statusCode == 200 && body.stream) {
-			sidebar[1] = "[● DEVWARS LIVE](http://www.twitch.tv/DevWars)";
-			r.getSubreddit("DevWars").editSettings({"description": sidebar.join("[](#" + c.reddit.subreddit + ")")});
-		} else {
-			request({
-				url: "http://devwars.tv/v1/game/nearestgame",
-				json: true
-			}, (error, response, body) => {
-				if(!error && response.statusCode == 200 && body.timestamp) {
-					var d = new Date(body.timestamp);
-					sidebar[1] = "**Next DevWars:**[](#linebreak) *" + days[d.getUTCDay()] + ", " + months[d.getUTCMonth()] + " " + d.getUTCDate() + " - " + d.getUTCHours() % 12 + ":" + d.getUTCMinutes() + " " + (d.getUTCHours() - 12 < 0 ? "AM" : "PM") + " UTC*";
-				} else {
-					sidebar[1] = "**Next DevWars:**[](#linebreak) *Unavailable*";
-				}
-
-				r.getSubreddit("DevWars").editSettings({"description": sidebar.join("[](#" + c.reddit.subreddit + ")")});
-			});
+let processResponse = res => {
+	return new Promise((resolve, reject) => {
+		if(!res.ok) reject(response.statusText);
+		else {
+			res.json().then(resolve).catch(reject);
 		}
 	});
-});
+};
+
+r.getSubreddit(c.reddit.subreddit).getSettings().then(res => {
+	sidebar = res.description.split("[](#" + c.reddit.subreddit + ")");
+	if(sidebar.length != 3) throw new Error("Sidebar tag mismatch");
+
+	return Promise.all([
+		fetch(`https://api.twitch.tv/kraken/streams/${c.twitch.channel}`, {
+			headers: new fetch.Headers({
+				"Accept": "application/vnd.twitchtv.v5+json",
+				"Client-ID": c.twitch.clientid
+			})
+		}),
+		fetch(`https://devwars.tv/v1/game/nearestgame`)
+	]);
+}).then((res) => Promise.all(res.map(processResponse))).then(([twitch, devwars]) => {
+	if(twitch.stream) {
+		sidebar[1] = "[● DEVWARS LIVE](https://www.twitch.tv/DevWars)";
+	} else {
+		sidebar[1] = "**Next DevWars:**[](#linebreak) ";
+		if(devwars.timestamp) {
+			let d = new Date(devwars.timestamp);
+			sidebar[1] += "*" + days[d.getUTCDay()] + ", " + months[d.getUTCMonth()] + " " + d.getUTCDate() + " - " + ((d.getUTCHours() % 12) < 10 ? "0" : "") + (d.getUTCHours() % 12) + ":" + (d.getUTCMinutes() < 10 ? "0" : "") + d.getUTCMinutes() + " " + (d.getUTCHours() - 12 < 0 ? "AM" : "PM") + " UTC*";
+		} else {
+			sidebar[1] += "*Unavailable*";
+		}
+	}
+
+	r.getSubreddit("DevWars").editSettings({"description": sidebar.join("[](#" + c.reddit.subreddit + ")")});
+}).catch(console.error);
